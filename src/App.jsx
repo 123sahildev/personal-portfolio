0471bec94bd3dcbbcf6a04d7922114efc66302b4
sahilzahoor <hey.sahilzahoor@gmail.com>
2026-02-08 23:10:58 +0500

    again check headering

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
    <>
     <div className="absolute top-0  left-0 h-15 w-full bg-[darkblue] flex items-center">
-      <h1 className="ml-4 text-[30px] lg:block hidden font-bold text-white font-[Arial]">Sahil portfolio</h1>
+      <h1 className="ml-4 text-[30px] font-bold text-white font-[Arial]">Sahil portfolio</h1>
     </div>
    </>
   )
