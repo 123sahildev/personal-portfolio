ffde2b60d2f09e028fea7591d907173c3f4bdeb7
sahilzahoor <hey.sahilzahoor@gmail.com>
2026-02-09 12:18:44 +0500

    Second last commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import React from 'react'
+
+export default function App() {
+  return (
+   <>
+    <div className="absolute top-0  left-0 h-15 w-full bg-[darkblue] flex items-center">
+      <h1 className="ml-4 text-[30px] font-bold text-white font-[Arial]">Sahil portfolio</h1>
+    </div>
+    <button>New button added</button>
+   </>
+  )
+}
