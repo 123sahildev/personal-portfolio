6e107c2133686143d4c46052fe2dd502e0fc4b19
sahilzahoor <hey.sahilzahoor@gmail.com>
2026-02-09 13:53:05 +0500

    background changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import React from 'react'
 export default function App() {
   return (
    <>
-    <div className="absolute top-0  left-0 h-10 lg:h-15 w-full the bg-[darkblue] flex items-center">
-      <h1 className="ml-4 text-[20px] lg:text-[30px] font-bold text-white font-[Arial]">SahilCode<span className="text-[#00eeff]">X</span></h1>
+    <div className="fixed z-10 top-0  left-0 h-10 lg:h-15 w-full the border  border-b-[#8b8989a9] bg-[#01011f] flex items-center">
+      <h1 className="ml-4 text-[20px] lg:text-[30px] font-bold text-white font-[Arial] ">SahilCode<span className="text-[#00eeff]">X</span></h1>
+    </div>
+    <div className="absolute lg:top-15 top-10 bg-linear-to-br to-[darkblue] from-20% from-[#00001d] w-full h-full">
+      
     </div>
    </>
-  )
+  );
 }
